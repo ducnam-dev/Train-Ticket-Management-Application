efe9832cea8a3596d35e0ec2e43984ed80eb302b
TranSone-Code <23651831.son@student.iuh.edu.vn>
2025-12-23 05:41:15 +0700

    ép kiểu giao diện Metal trong Main.java để máy Mac hiển thị giống giao diện bên Win

--- src/view/Main.java
+++ src/view/Main.java
@@ -1,12 +1,17 @@
 package view;
 
-import javax.swing.SwingUtilities;
+import javax.swing.*;
 
 import gui.MainFrame.ManHinhDangNhap;
 
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
+            try {
+                UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
             new ManHinhDangNhap().setVisible(true);
         });
     }
